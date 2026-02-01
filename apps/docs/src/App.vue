9ac4a3e6bb0367e1521fa39ac4930e44366385c4
路狐羽 <75880483+aixcyi@users.noreply.github.com>
2026-02-01 23:51:03 +0800

    refactor(docs): 稍微装点“文档月饼盒”
    1、添加卡片滑移效果；
    2、添加月饼盒底（带二维码那个）；
    3、移除开始按钮，改成为有文档的项目提供按钮；
    4、添加并渲染（左下角）项目类型图标。

--- apps/docs/src/App.vue
+++ apps/docs/src/App.vue
@@ -1,55 +1,174 @@
 <script lang="ts" setup>
-import FlairButton from '#/components/FlairButton.vue'
-import RawCard from '#/components/RawCard.vue'
-import RepoCard from '#/components/RepoCard.vue'
-import { Icon } from '@iconify/vue'
-import { projects } from '@navifox/constants'
+import FlairButton from '#/components/FlairButton.vue';
+import { Icon } from '@iconify/vue';
+import { copyrightInterval, documentation, navifoxGuild, projects } from '@navifox/constants';
+import { useTitle } from '@vueuse/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { range } from 'lodash-es';
+import { onMounted, onUnmounted } from 'vue';
+import { Vue3NextQrcode } from 'vue3-next-qrcode';
+
+useTitle().value = documentation.name
+
+gsap.registerPlugin(ScrollTrigger)
+
+function initializePanels() {
+    const panels: HTMLDivElement[] = gsap.utils.toArray("#panel")
+    if (panels.length == 0) return
+    panels.forEach(panel => {
+        ScrollTrigger.create({
+            trigger: panel,
+            start: "top top",
+            pin: true,
+            pinSpacing: false
+        })
+    })
+    const snapUnit = 1 / (panels.length - 1)
+    const trigger = ScrollTrigger.create({
+        snap(value) {
+            let snappedValue = gsap.utils.snap(snapUnit, value)
+            if (0 < snappedValue || snappedValue < 1) {
+                return snappedValue
+            }
+            return value
+        }
+    })
+    trigger.scroll(0)
+}
+
+function initializeArrows() {
+    gsap.to("#arrow", {
+        keyframes: {
+            y: [ 0, -8, 8, 0 ],
+            opacity: [ 1, .2, 1, 1 ],
+            ease: "none",
+            easeEach: "power3.out"
+        },
+        ease: "elastic",
+        duration: 2,
+        stagger: .25,
+        repeat: -1,
+    });
+}
+
+onMounted(() => {
+    setTimeout(initializePanels, 100)
+    setTimeout(initializeArrows, 100)
+})
+onUnmounted(() => {
+    ScrollTrigger.getAll().forEach(trigger => trigger.kill())
+})
 </script>
 
 
 <template>
-    <section class="w-screen min-h-screen flex justify-center items-center">
-        <RawCard class="gap-8 text-nowrap">
-            <h1 class="text-5xl text-white"><b>文档月饼盒</b></h1>
-            <span class="text-lg text-amber-100 tracking-[.1em]">· 收纳展示散落在各个项目仓库的文档 ·</span>
-            <div class="text-base font-mono relative">
-                <FlairButton flair-style="--button-stroke: var(--gradient-milktea);"
-                             href="#panel"
-                             use="a">
-                    &nbsp;开始浏览&nbsp;<br />
-                </FlairButton>
-            </div>
-        </RawCard>
+    <section id="panel"
+             class="w-full h-screen flex justify-center items-center"
+             style="background-color: var(--background)">
+        <div class="p-12 inline-flex flex-col items-center gap-8 text-nowrap">
+            <h1 class="text-5xl text-white"><b>{{ documentation.name }}</b></h1>
+            <span class="text-lg text-amber-100 tracking-[.1em]">· {{ documentation.desc }} ·</span>
+        </div>
+        <div class="absolute bottom-16 left-1/2" style="transform: translateX(-50%)">
+            <Icon v-for="() in range(3)"
+                  id="arrow"
+                  class="text-amber-100"
+                  height="24"
+                  icon="bytesize:chevron-bottom" />
+        </div>
     </section>
 
-    <section class="w-screen min-h-screen flex justify-center items-center flex-col">
-        <div id="panel" />
-        <RepoCard v-for="project in projects"
-                  :description="project.desc"
-                  :doc="project.docs"
-                  :title="project.name"
-                  class="gap-6 text-amber-100 LinkCard">
-            <template #iconbar>
+    <!-- var(--button-hoverColor) 也用 <FlairButton/> -->
+    <section v-for="(project, index) in projects"
+             id="panel"
+             :style="{'--button-hoverColor': index % 2 ? 'var(--color-cyan-950)' : 'var(--color-gray-800)'}"
+             class="w-full h-screen flex justify-center items-center flex-col"
+             style="background-color: var(--button-hoverColor)">
+        <div class="p-12 inline-flex flex-col items-center gap-6 text-amber-100 z-10">
+            <span class="text-3xl text-nowrap">{{ project.name }}</span>
+            <div class="inline-flex flex-row flex-nowrap items-center gap-4">
                 <template v-for="item in project.socials">
                     <a v-if="item.icon"
                        :href="item.link"
                        class="hover:text-amber-300">
-                        <Icon :icon="item.icon" height="24" />
+                        <Icon :icon="item.icon" height="32" />
                     </a>
                 </template>
-            </template>
-        </RepoCard>
+            </div>
+            <span class="max-w-sm indent-8 text-gray-300"
+                  v-html="project.desc" />
+            <div v-if="project.docLink"
+                 class="text-base font-mono relative">
+                <FlairButton :href="project.docLink"
+                             flair-style="--button-stroke: var(--gradient-milktea);"
+                             use="a">
+                    &nbsp;浏览文档&nbsp;<br />
+                </FlairButton>
+            </div>
+        </div>
+        <Icon v-if="project.typeIco"
+              :class="index % 2 ? 'text-gray-600' : 'text-gray-700'"
+              :icon="project.typeIco"
+              class="absolute md:left-16 md:bottom-16 left-8 bottom-8"
+              height="unset"
+              style="--size: calc(min(100vh, 100vw) / 3 + 1px); height: var(--size); width: var(--size)"
+              width="unset" />
+    </section>
+
+    <section id="panel"
+             class="w-full h-screen flex justify-center items-center"
+             style="background-color: var(--background)">
+        <div class="p-1.5 MooncakeRect"
+             style="background-color: var(--color-gray-400)">
+            <div class="p-12 inline-flex items-center flex-wrap gap-4 justify-center max-sm:flex-col MooncakeRect"
+                 style="background-color: var(--background)">
+                <div class="inline-flex flex-col text-nowrap gap-1 max-sm:text-center">
+                    <span class="text-md text-gray-400">产地：{{ navifoxGuild.name }}</span>
+                    <span class="text-md text-gray-400">生产日期：{{ copyrightInterval.start }}年</span>
+                    <span class="text-md text-gray-400">保质期：{{ copyrightInterval.start + 1000 }}年</span>
+                    <span class="text-md text-gray-400">包装规格：{{ projects.length }} 个/盒</span>
+                </div>
+                <Vue3NextQrcode
+                    :size="128"
+                    :text="navifoxGuild.link"
+                    :whiteMargin="false"
+                    colorDark="#99a1af"
+                    colorLight="transparent" />
+            </div>
+        </div>
     </section>
 </template>
 
 
 <style scoped>
-.LinkCard {
-    border: 1px solid transparent;
-
-    &:hover {
-        border-top: 1px solid var(--color-orange-400);
-        border-bottom: 1px solid var(--color-orange-400);
-    }
+.MooncakeRect {
+    mask: radial-gradient(
+        circle at calc(100% + 10px) calc(100% + 10px),
+        transparent 0,
+        transparent 24px,
+        #2179f5 25px
+    ),
+    radial-gradient(
+        circle at -10px -10px,
+        transparent 0,
+        transparent 24px,
+        #2179f5 25px
+    ),
+    radial-gradient(
+        circle at calc(100% + 10px) -10px,
+        transparent 0,
+        transparent 24px,
+        #2179f5 25px
+    ),
+    radial-gradient(
+        circle at -10px calc(100% + 10px),
+        transparent 0,
+        transparent 24px,
+        #2179f5 25px
+    );
+    mask-repeat: no-repeat;
+    mask-position: right bottom, left top, right top, left bottom;
+    mask-size: 70% 70%;
 }
 </style>
