73bc5e6de9f965ad59ea216f46359a2b3f5bef43
路狐羽 <75880483+aixcyi@users.noreply.github.com>
2026-07-18 23:04:03 +0800

    refactor: 打包工具 vite-plugin-zip-file 替换为 bestzip

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,24 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import * as path from 'node:path';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import { viteZip } from 'vite-plugin-zip-file';
 
 const timestamp = Math.trunc(new Date().getTime() / 1000);
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        vue(),
-        viteZip({
-            folderPath: path.resolve(__dirname, 'dist'),
-            outPath: path.resolve(__dirname),
-            zipName: 'dist.zip',
-            withoutMainFolder: true,
-        }),
-    ],
+    plugins: [tailwindcss(), vue()],
     define: {
         __APP_VERSION__: `"v${process.env.npm_package_version}+${timestamp}"`,
     },
